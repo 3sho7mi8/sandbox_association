bab9f496dd007aa21ed5525d21fb263ef3402e57
3sho7mi8 <hiro.pinot@gmail.com>
2018-05-29 22:43:56 +0900

    Add distinct to user and medium models

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,5 @@
 class Medium < ApplicationRecord
   has_many :user_media
-  has_many :users, through: :user_media
+  has_many :users, -> { distinct }, through: :user_media
   has_many :vaults
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :user_media
-  has_many :media, through: :user_media
+  has_many :media, -> { distinct }, through: :user_media
 end
