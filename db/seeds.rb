c883e8404d2079db5618a67a7418415d8aa90ffa
3sho7mi8 <hiro.pinot@gmail.com>
2018-05-29 22:30:07 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_lists = %i(
+  'yoshino'
+  'tanaka'
+  'kouchi'
+  'urawa'
+  'hotta'
+)
+
+user_lists.each do |u|
+  User.create!(name: u)
+end
+
+
+media_lists = %i(
+  'mynavi'
+  'rycnavi'
+  'en'
+  'wantedly'
+)
+
+media_lists.each do |m|
+  Medium.create!(name: m)
+end
